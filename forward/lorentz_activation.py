362ae47181597a33b1d0897438bbb5eb2bfbc280
amahendra98 <32529900+amahendra98@users.noreply.github.com>
2020-04-03 04:59:18 -0400

    Update lorentz_activation.py

--- forward/lorentz_activation.py
+++ forward/lorentz_activation.py
@@ -11,7 +11,7 @@ import numpy as np
 
 # Pytorch module
 import torch.nn as nn
-#import torch                               #<- This line may or may not be necessary
+import torch
 from torch import pow, add, mul, div, sqrt
 
 def lorentzActivation(w, w0, wp, g):
@@ -19,11 +19,45 @@ def lorentzActivation(w, w0, wp, g):
   e2 = div(mul(mul(pow(wp,2),w),g), add(pow(add(pow(w0,2),-w),2), pow(mul(w,g),2)))
   return e2
   
+  
 class LAF(nn.Module):
+  
+"""
+Lorentz Activation Function with preset, fixed parameters
+"""
+
   def __init__(self):
     super(LAF, self).__init__()
     
   def forward(self,w):
     return lorentzActivation(w,0,wp,g)  #assume w0 = 0, other values must also be predefined
+
+  
+  
+class LLAF(nn.Module):
+  
+"""
+Learnable Lorentz Activation function without preset parameters, except w0 = 0
+"""
+
+  def __init__(self, g = None, wp = None):
+    super(LLAF, self).__init__()
+    
+    # Save g and wp as tracked parameters of this function module
+    if g == None:
+      self.g = Parameter(torch.tensor(0.0))
+    else:
+      self.g = Paramter(torch.tensor(g))
+      
+    if wp == None:
+      self.wp = Parameter(torch.tensor(0.0))
+    else:
+      self.wp = Paramter(torch.tensor(wp))
+    
+    self.g.requiresGrad = True
+    self.wp.requiresGrad = True
+    
+  def forward(self,w):
+    return lorentzActivation(w,0,self.wp,self.g)  #assume w0 = 0
     
   
